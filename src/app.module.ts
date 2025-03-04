f350236e78d052f165d37bc6ee9b8098d0013905
Rak <sivping82@gmail.com>
2025-03-04 23:07:33 +0700

    refactor: remove Books module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/user.module';
-import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UserModule, BooksModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
