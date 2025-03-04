f0be91f7bb03837b18e37b799e45665e219258f0
Rak <sivping82@gmail.com>
2025-03-05 04:05:52 +0700

    feat: implement user controller with various endpoints and add simple middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/user.module';
+import { SimpleMiddleware } from './middleware/simple.middlewares';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(SimpleMiddleware)
+      .forRoutes({ path: 'users/*', method: RequestMethod.GET });
+  }
+}
